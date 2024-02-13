9b558a1f7d19677d58d67a7cc31dfa1df9b1a761
Nina Weber <weber_nina@web.de>
2024-02-13 12:56:37 +0100

    changes progress-bar

--- js-functions-2_progress-bar/js/index.js
+++ js-functions-2_progress-bar/js/index.js
@@ -21,4 +21,17 @@ For that:
 
 const progressBar = document.querySelector('[data-js="progress-bar"]');
 
-function calculateScrollPercentage() {}
+function calculateScrollPercentage() {
+  const scrollY = window.scrollY;
+  const windowHeight = window.innerHeight;
+  const webpageHeight = document.body.clientHeight;
+  const viewPortFromBottom = scrollY + windowHeight;
+  const scrollPercentage = (viewPortFromBottom / webpageHeight) * 100;
+  return `${scrollPercentage}%`;
+}
+
+progressBar.style.width = calculateScrollPercentage();
+
+document.addEventListener("scroll", () => {
+  progressBar.style.width = calculateScrollPercentage();
+});
