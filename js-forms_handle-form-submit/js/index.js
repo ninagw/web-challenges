a6f1a74c634c61d701b1bcf5fb3b4729baa32f2f
Nina Weber <weber_nina@web.de>
2024-02-13 16:02:06 +0100

    change console.log

--- js-forms_handle-form-submit/js/index.js
+++ js-forms_handle-form-submit/js/index.js
@@ -7,7 +7,15 @@ form.addEventListener("submit", (event) => {
 
   const formElements = event.target.elements;
 
-  console.log(event.target.elements);
+  console.log(formElements.firstName.value);
+  console.log(formElements.lastName.value);
+  console.log(formElements.age.value);
+  console.log(formElements.email.value);
+  console.log(formElements.complaint.value);
+  console.log(formElements.details.value);
+  console.log(formElements.badness.value);
+  console.log(formElements.orderDate.value);
+  console.log(formElements.tos.value);
 
   event.target.reset();
 
