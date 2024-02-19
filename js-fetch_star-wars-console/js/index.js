7d3f35e7709a2f5aa281e625c74c8cba23fe1a10
Nina Weber <weber_nina@web.de>
2024-02-19 16:28:04 +0100

    add js-fetch_star-wars-console

---
+++ js-fetch_star-wars-console/js/index.js
@@ -0,0 +1,25 @@
+console.clear();
+
+const url = "https://swapi.dev/api/people";
+
+async function fetchData() {
+  const response = await fetch("https://swapi.dev/api/people");
+
+  const data = await response.json();
+
+  console.log(response);
+  console.log(data);
+
+  // complete array of objects
+  console.log(data.results);
+
+  // one star wars character by using its index (object of R2-D2)
+  console.log(data.results[2]);
+
+  // log eye color of R2-D2:
+  console.log(data.results[2].eye_color);
+
+  return data;
+}
+
+fetchData();
