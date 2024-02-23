ae290b8191930ed0be0660365bcb01915b580457
Nina Weber <weber_nina@web.de>
2024-02-23 11:35:34 +0100

    add li-element for astronauts in space

--- people-in-space/script.js
+++ people-in-space/script.js
@@ -1,6 +1,6 @@
 const peopleInSpace = document.querySelector('[data-js="people-in-space"]');
-const namesOfAstronauts = document.querySelector(
-  '[data-js="names-of-astronauts"]'
+const listOfAstronauts = document.querySelector(
+  '[data-js="list-of-astronauts"]'
 );
 
 const people = [
@@ -53,20 +53,20 @@ async function getDataFromAPI() {
   const data = await response.json();
   console.log(data);
   peopleInSpace.textContent = data.number;
-
-  // create li-element for each astronaut
-  const allAstronauts = data.people;
-  allAstronauts.forEach((astronaut) => {
-    const listElement = document.createElement("li");
-    listElement.appendChild(namesOfAstronauts);
-    listElement.textContent = data.people.name;
-  });
 }
 
 getDataFromAPI();
 
-peopleInSpace.forEach((astronaut) => {
+// create li-element for each astronaut
+const allAstronauts = people;
+allAstronauts.forEach((astronaut) => {
   const listElement = document.createElement("li");
-  listElement.appendChild(namesOfAstronauts);
-  listElement.textContent = data.people.name;
+  listElement.appendChild(listOfAstronauts);
+  listElement.textContent = `${people.name}`;
 });
+
+// peopleInSpace.forEach((astronaut) => {
+//   const listElement = document.createElement("li");
+//   listElement.appendChild(listOfAstronauts);
+//   listElement.textContent = data.people.name;
+// });
