1a975c02d79515b52b678a9c777eb28027e9325c
Nina Weber <weber_nina@web.de>
2024-02-27 16:56:06 +0100

    add solution

--- react-state-2_table-reservation/src/App.js
+++ react-state-2_table-reservation/src/App.js
@@ -1,12 +1,26 @@
 import React from "react";
 import Counter from "./components/Counter";
 import "./styles.css";
+import { useState } from "react";
 
 export default function App() {
+  const [people, setPeople] = useState(0);
+
+  function handleAddPeople() {
+    setPeople(people + 1);
+  }
+
+  function handleSubtractPeople() {
+    setPeople(people - 1);
+  }
+
   return (
     <div className="container">
       <h1>Place a Table Reservation</h1>
-      <Counter />
+      <Counter
+        onAddPeople={handleAddPeople}
+        onSubtractPeople={handleSubtractPeople}
+      />
       <p>You are going to reserve a table for 2 people.</p>
     </div>
   );
