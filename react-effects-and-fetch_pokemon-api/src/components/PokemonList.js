4982a106fd5dc9e789ac3833d5f51f0cbd8f12f0
Nina Weber <weber_nina@web.de>
2024-02-28 15:26:00 +0100

    add solution

--- react-effects-and-fetch_pokemon-api/src/components/PokemonList.js
+++ react-effects-and-fetch_pokemon-api/src/components/PokemonList.js
@@ -1,23 +1,28 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function PokemonList() {
   const [pokemon, setPokemon] = useState([]);
+  // const [data, setData] = useState();
 
-  async function loadPokemon() {
-    try {
-      const response = await fetch("https://pokeapi.co/api/v2/pokemon");
-      const data = await response.json();
-      setPokemon(data.results);
-    } catch (error) {
-      console.log(error);
+  useEffect(() => {
+    async function loadPokemon() {
+      try {
+        const response = await fetch("https://pokeapi.co/api/v2/pokemon");
+        const data = await response.json();
+        setPokemon(data.results); // fügt neue Pokemons hinzu
+      } catch (error) {
+        console.log(error); // log error, wenn API nicht erreichbar oder anderer Fehler auftritt
+      }
     }
-  }
+
+    loadPokemon(); // calls the loadPokemon function and fetches the data from the API
+  }, []);
 
   return (
     <main>
-      <button type="button" onClick={loadPokemon}>
+      {/* <button type="button" onClick={loadPokemon}>
         Load Pokémon
-      </button>
+      </button> */}
       <ul>
         {pokemon.map(({ name }) => (
           <li key={name}>{name}</li>
