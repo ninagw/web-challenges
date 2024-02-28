b23cbf78fc382e01a26ef8e8c270501377b455f8
Nina Weber <weber_nina@web.de>
2024-02-28 12:21:06 +0100

    add solution

--- react-state-3_adding-animals/src/App.js
+++ react-state-3_adding-animals/src/App.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import "./styles.css";
 import Form from "./components/Form/index.js";
 import List from "./components/List/index.js";
+import { uid } from "uid";
 
 const initialAnimals = [
   {
@@ -24,8 +25,18 @@ const initialAnimals = [
 export default function App() {
   const [animals, setAnimals] = useState(initialAnimals);
 
-  function handleAddAnimal(newAnimal) {
-    console.log(newAnimal);
+  function handleAddAnimal() {
+    // console.log(newAnimal);
+    event.preventDefault();
+
+    const formElements = event.target.elements;
+    const animalName = formElements.name.value; // speichert den Tiernamen, der neu im Formular eingegeben wurde
+    const animalEmoji = formElements.emoji.value; // speichert das Emoji, das neu im Formular ausgewählt wurde
+
+    setAnimals([
+      ...animals, // erstellt Kopie aller Tiere aus meinem ursprünglichen Array "initialAnimals"
+      { id: uid(), name: animalName, emoji: animalEmoji }, // erstellt neues Tier-Objekt und fügt sie dem kopierten Animal-Array hinzu.
+    ]);
   }
 
   return (
