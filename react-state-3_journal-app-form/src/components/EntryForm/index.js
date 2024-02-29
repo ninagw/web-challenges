440f264ea22ae96d9717ed32d5321a31e78e3a85
Nina Weber <weber_nina@web.de>
2024-02-29 19:39:46 +0100

    add solution, not done yet

--- react-state-3_journal-app-form/src/components/EntryForm/index.js
+++ react-state-3_journal-app-form/src/components/EntryForm/index.js
@@ -1,7 +1,24 @@
 import "./EntryForm.css";
 import Button from "../Button";
 
-export default function EntryForm() {
+export default function EntryForm({ onAddEntry }) {
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    const data = Object.fromEntries(formData); // all values of all input fields
+
+    const date = new Date().toLocaleDateString("en-us", {
+      dateStyle: "medium",
+    });
+    // const newObject = { motto: data.motto.values, notes: data.notes.values };
+    data.date = date;
+    onAddEntry(date);
+
+    event.target.reset();
+    event.target.elements.motto.focus();
+  }
+
   return (
     <form className="entry-form">
       <h2 className="entry-form__title">New Entry</h2>
@@ -15,7 +32,9 @@ export default function EntryForm() {
           <textarea name="notes" id="notes" rows="4" />
         </div>
         <div className="entry-form__button-wrapper">
-          <Button type="submit">Create</Button>
+          <Button type="submit" onSubmit={handleSubmit}>
+            Create
+          </Button>
         </div>
       </div>
     </form>
