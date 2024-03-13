81f6137f70cfaebd6645e76957249970a0fdfaf6
Nina Weber <weber_nina@web.de>
2024-03-13 11:32:17 +0100

    add solution

--- backend-basics_chance/server.js
+++ backend-basics_chance/server.js
@@ -1,3 +1,15 @@
 import { createServer } from "node:http";
+import { Chance } from "chance";
 
-// export const server = …
+const chance = new Chance();
+
+export const server = createServer((request, response) => {
+  const name = chance.name();
+  const age = chance.age();
+  const profession = chance.profession();
+
+  response.status = 200;
+  response.end(
+    `Hello, my name is ${name} and I am ${age} years old. I am a ${profession}.`
+  );
+});
