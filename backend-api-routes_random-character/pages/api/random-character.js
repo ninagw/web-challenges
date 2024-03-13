230733869ad99a2d7c861367fdc9b1d397424044
Nina Weber <weber_nina@web.de>
2024-03-13 20:34:58 +0100

    add frontend

--- backend-api-routes_random-character/pages/api/random-character.js
+++ backend-api-routes_random-character/pages/api/random-character.js
@@ -2,13 +2,12 @@ import Chance from "chance";
 
 const chance = new Chance();
 const character = {
-    firstName: chance.first();
-    lastName: chance.last();
-    twitterName: chance.twitter();
-    geohash: chance.geohash();
+  firstName: chance.first(),
+  lastName: chance.last(),
+  twitterName: chance.twitter(),
+  geohash: chance.geohash(),
 };
 
-export default function handler() {
-
-  response.status(200).json();
+export default function handler(request, response) {
+  response.status(200).json(character);
 }
--- backend-api-routes_random-character/pages/index.js
+++ backend-api-routes_random-character/pages/index.js
@@ -1,3 +1,31 @@
+import useSWR from "swr";
+
+const fetcher = (url) => fetch(url).then((response) => response.json());
+
 export default function HomePage() {
-  return <h1>Hello from Next.js!</h1>;
+  const {
+    data: randomCharacter,
+    isLoading,
+    error,
+  } = useSWR("/api/random-character", fetcher);
+
+  if (!randomCharacter) {
+    return;
+  }
+
+  if (isLoading) {
+    return <>Loading...</>;
+  }
+
+  const { firstName, lastName, twitterName, geohash } = randomCharacter;
+
+  return (
+    <>
+      <h1>
+        {firstName} {lastName}
+      </h1>
+      <p>Twitter: {twitterName}</p>
+      <p>Geohash: {geohash}</p>
+    </>
+  );
 }
