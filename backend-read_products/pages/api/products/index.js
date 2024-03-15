7d60c76d26c856680fa87763c1cf377a0732356f
Nina Weber <weber_nina@web.de>
2024-03-15 17:18:25 +0100

    add task 1

--- backend-read_products/pages/api/products/index.js
+++ backend-read_products/pages/api/products/index.js
@@ -1,5 +1,16 @@
-import { products } from "../../../lib/products";
+// import { products } from "../../../lib/products";
+import dbConnect from "@/db/connect";
+import Product from "@/db/models/Product";
 
-export default function handler(request, response) {
-  return response.status(200).json(products);
+export default async function handler(request, response) {
+  await dbConnect();
+
+  if (request.method === "GET") {
+    const products = await Product.find();
+    // console.log("Products: ", products);
+
+    return response.status(200).json(products);
+  } else {
+    return response.status(405).json({ message: "Method not allowed" });
+  }
 }
