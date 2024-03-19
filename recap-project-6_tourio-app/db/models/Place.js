8e17692094c7d052a4784768debf8d19c943cd58
Nina Weber <weber_nina@web.de>
2024-03-19 10:35:21 +0100

    almost done

--- recap-project-6_tourio-app/db/models/Place.js
+++ recap-project-6_tourio-app/db/models/Place.js
@@ -5,9 +5,9 @@ const { Schema } = mongoose;
 const placeSchema = new Schema({
   name: { type: String, required: true },
   location: { type: String, required: true },
-  image: { type: String, required: true },
-  mapURL: { type: String, required: true },
-  description: { type: String, required: true },
+  image: { type: String, required: false },
+  mapURL: { type: String, required: false },
+  description: { type: String, required: false },
 });
 
 const Place = mongoose.models.Place || mongoose.model("Place", placeSchema);
--- recap-project-6_tourio-app/pages/api/places/[id]/index.js
+++ recap-project-6_tourio-app/pages/api/places/[id]/index.js
@@ -11,30 +11,23 @@ export default async function handler(request, response) {
   }
 
   if (request.method === "GET") {
-    try {
-      const place = await Place.findById(id);
-      return response.status(200).json(place);
-    } catch (error) {
+    const place = await Place.findById(id);
+
+    if (!place) {
       return response.status(404).json({ error: "Not found" });
     }
+
+    return response.status(200).json(place);
   }
 
   if (request.method === "PATCH") {
-    try {
-      const updatePlace = request.body;
-      await Place.findByIdAndUpdate(id, updatePlace);
-      return response.status(200).json({ message: `Place ${id} updated!` });
-    } catch (error) {
-      return response.status(404).json({ error: "Not found." });
-    }
+    const updatePlace = request.body;
+    await Place.findByIdAndUpdate(id, updatePlace);
+    return response.status(200).json({ message: `Place ${id} updated!` });
   }
 
   if (request.method === "DELETE") {
-    try {
-      await Place.findByIdAndDelete(id);
-      return response.status(200).json({ message: `Place ${id} deleted!` });
-    } catch (error) {
-      return response.status(404).json({ error: "Not found." });
-    }
+    await Place.findByIdAndDelete(id);
+    return response.status(200).json({ message: `Place ${id} deleted!` });
   }
 }
