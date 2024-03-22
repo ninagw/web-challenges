e7e9b4db3cb0d6056172dfba2ed6388798efb09e
Nina Weber <weber_nina@web.de>
2024-03-22 09:55:31 +0100

    display data

--- crud-recap/pages/index.js
+++ crud-recap/pages/index.js
@@ -1,4 +1,9 @@
+import initialPlaces from `@/lib/db.js`;
+import { useState } from "react";
+
 export default function HomePage() {
+const [places, setPlaces] = useState(initialPlaces);
+
   return (
     <div>
       <h1>Hello from Next.js</h1>
