5ac47e8a8121709f78fe68f13adfe7650a09fce6
Nina Weber <weber_nina@web.de>
2024-03-23 17:57:47 +0100

    update _app.js

--- crud-recap/pages/_app.js
+++ crud-recap/pages/_app.js
@@ -2,19 +2,15 @@ import GlobalStyle from "../styles";
 import { initialPlaces } from "@/lib/db";
 import { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
-import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }) {
   const [places, setPlaces] = useState(initialPlaces);
   // console.log("INITIAL_PLACES: ", places);
-  const router = useRouter();
 
   function handleAddPlace(newPlaceData) {
     const newPlace = { id: uuidv4(), newPlaceData };
 
     setPlaces([...places, newPlace]);
-
-    router.push("/");
   }
 
   // JANAs Ansatz:
