4ca9c7d6448bb2536d69fa79f590915a010477a1
Nina Weber <weber_nina@web.de>
2024-05-08 12:46:15 +0200

    add solution

--- js-functions_dom-manipulation-2/js/index.js
+++ js-functions_dom-manipulation-2/js/index.js
@@ -14,25 +14,34 @@ We want to add some functionality to the buttons on this page. When pressing a b
 redButton.addEventListener("click", () => {
   // use your functions to
   // - remove all classes
+  removeAllColors();
   // - then add the class name: "red"
+  addColor("red");
 });
 
 blueButton.addEventListener("click", () => {
-  // use your functions to
-  // - remove all classes
-  // - then add the class name: "blue"
+  removeAllColors();
+  addColor("blue");
 });
 
 greenButton.addEventListener("click", () => {
-  // use your functions to
-  // - remove all classes
-  // - then add the class name: "green"
+  removeAllColors();
+  addColor("green");
 });
 
 grayButton.addEventListener("click", () => {
-  // use your functions to
-  // - remove all classes
-  // - then add the class name: "gray"
+  removeAllColors();
+  addColor("gray");
 });
 
 // Write your two functions below:
+function removeAllColors() {
+  box.classList.remove("red");
+  box.classList.remove("green");
+  box.classList.remove("blue");
+  box.classList.remove("gray");
+}
+
+function addColor(color) {
+  box.classList.add(color);
+}
