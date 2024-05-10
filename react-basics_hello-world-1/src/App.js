b8b72f71fa8617725b82afa177ca38ec6ae35c38
Nina Weber <weber_nina@web.de>
2024-05-10 17:35:18 +0200

    add solution / react basic challenge

---
+++ react-basics_hello-world-1/src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./styles.css";
+
+function HelloWorld() {
+  return <h1>Hello react world!</h1>;
+}
+
+export default function App() {
+  return <HelloWorld />;
+}
