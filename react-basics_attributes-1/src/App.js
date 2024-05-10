c9b534f112e569e25146e1d8457675ef8316095a
Nina Weber <weber_nina@web.de>
2024-05-10 17:42:30 +0200

    add solution / react attributes

--- react-basics_attributes-1/src/App.js
+++ react-basics_attributes-1/src/App.js
@@ -1,6 +1,17 @@
 import React from "react";
 import "./styles.css";
 
+function Article() {
+  <article>
+    <h2 className="article__title">Harry Potter Quiz</h2>
+    <label id="x"></label>
+    <input htmlFor="x"></input>
+    <a className="article__link" href="/">
+      Link
+    </a>
+  </article>;
+}
+
 export default function App() {
-  return <div>Replace me with an article</div>;
+  return <Article />;
 }
