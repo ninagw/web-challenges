11f4df48e8301e3542565f5dca417dc6a773cd8a
Nina Weber <weber_nina@web.de>
2024-05-27 15:31:07 +0200

    update solution Math.round()

--- the-app-brewery-bootcamp/bmi-calculator.js
+++ the-app-brewery-bootcamp/bmi-calculator.js
@@ -7,7 +7,7 @@
 
 function bmiCalculator(weight, height) {
   const bmi = weight / (height * height);
-  return Math.floor(bmi);
+  return Math.round(bmi);
 }
 
 // Your challenge is to create a function that takes weight and height as inputs and gives the calculated BMI value as an output. The output should be rounded to the nearest whole number.
