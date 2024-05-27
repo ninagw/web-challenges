0f2a829a79704fdd501085c38860dc07c2758b8e
Nina Weber <weber_nina@web.de>
2024-05-27 14:29:20 +0200

    add solution

--- the-app-brewery-bootcamp/exercise-variables.js
+++ the-app-brewery-bootcamp/exercise-variables.js
@@ -1,8 +1,8 @@
 // Javascript Variables Exercise
 // Given the existing code below, can you write some code so that their values are switched around?
 
-// var a = "3";
-// var b = "8";
+var a = "3";
+var b = "8";
 // So that the variable a holds the value "8".
 
 // And the variable b holds the value "3".
@@ -12,6 +12,9 @@
 // a is 8
 
 // b is 3
+var c = a;
+a = b;
+b = c;
 
 // Do NOT change any of the existing code.
 
