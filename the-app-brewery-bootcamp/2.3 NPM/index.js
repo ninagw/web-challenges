98d48f365ef56814274a297307e4d44907644d9a
Nina Weber <weber_nina@web.de>
2024-08-26 18:39:24 +0200

    update index.js

--- the-app-brewery-bootcamp/2.3 NPM/index.js
+++ the-app-brewery-bootcamp/2.3 NPM/index.js
@@ -1,4 +1,6 @@
-var generateName = require("sillyname");
+// var generateName = require("sillyname");
+
+import generateName from "sillyname";
 var sillyName = generateName();
 
 console.log(`My name is ${sillyName}.`);
