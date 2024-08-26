6752b00034988650dece26631508c472157dce28
Nina Weber <weber_nina@web.de>
2024-08-26 18:10:23 +0200

    add content message.txt

--- the-app-brewery-bootcamp/2.2 Native Modules/index.js
+++ the-app-brewery-bootcamp/2.2 Native Modules/index.js
@@ -1,9 +1,9 @@
 const fs = require("fs");
 
-// fs.writeFile("message.txt", "Hello from NodeJS", (error) => {
-//   if (error) throw error;
-//   console.log("The file has been saved!");
-// });
+fs.writeFile("message.txt", "Hello from NodeJS", (error) => {
+  if (error) throw error;
+  console.log("The file has been saved!");
+});
 
 fs.readFile("./message.txt", "utf8", (err, data) => {
   if (err) throw err;
