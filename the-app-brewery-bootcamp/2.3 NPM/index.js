0dff4a75631dc74f577818de8dc27f3070b4337c
Nina Weber <weber_nina@web.de>
2024-08-26 18:51:10 +0200

    add npm superheroes

--- the-app-brewery-bootcamp/2.3 NPM/index.js
+++ the-app-brewery-bootcamp/2.3 NPM/index.js
@@ -1,6 +1,11 @@
 // var generateName = require("sillyname");
 
-import generateName from "sillyname";
-var sillyName = generateName();
+// import generateName from "sillyname";
+// var sillyName = generateName();
 
-console.log(`My name is ${sillyName}.`);
+// console.log(`My name is ${sillyName}.`);
+
+import superheroes from "superheroes";
+const superheroName = randomSuperhero();
+
+console.log(`I am superhero ${superheroName}!`);
