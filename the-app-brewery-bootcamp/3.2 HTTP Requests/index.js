bb2262ef8b3187202725b288b0cf27f25c624318
Nina Weber <weber_nina@web.de>
2024-08-29 16:18:24 +0200

    update 3.1 Express module and add module 3.3 Postman

--- the-app-brewery-bootcamp/3.2 HTTP Requests/index.js
+++ the-app-brewery-bootcamp/3.2 HTTP Requests/index.js
@@ -3,15 +3,19 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("<h1>Hello</h1>");
-}); // requests Homepage "/" and sends back the response "Hello!" which is rendered on the Hompage
+  res.send("<h1>Homepage</h1>");
+}); // requests Homepage "/" and sends back the response "Homepage" which is rendered on the Hompage
 
 app.get("/about", (req, res) => {
-  res.send("<h1>About Me</h1><p>My name is Angela</p>");
+  res.send("<h1>About Me</h1><p>My name is Nina</p>");
 });
 
 app.get("/contact", (req, res) => {
-  res.send("<h1>Contact Me</h1><p>Phone: +44123456789</p>");
+  res.send("<h1>Contact Me</h1><p>Phone: +0173-1234567</p>");
+});
+
+app.get("/impressum", (req, res) => {
+  res.send("<h1>Impressum</h1><p>Address: Munich</p>");
 });
 
 app.listen(port, () => {
