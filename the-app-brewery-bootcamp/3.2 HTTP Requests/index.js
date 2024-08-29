711a49eb541285038190c860743c272c832e40ba
Nina Weber <weber_nina@web.de>
2024-08-29 15:43:33 +0200

    install npm and express

--- the-app-brewery-bootcamp/3.2 HTTP Requests/index.js
+++ the-app-brewery-bootcamp/3.2 HTTP Requests/index.js
@@ -4,7 +4,7 @@ const port = 3000;
 
 app.get("/", (req, res) => {
   res.send("<h1>Hello</h1>");
-});
+}); // requests Homepage "/" and sends back the response "Hello!" which is rendered on the Hompage
 
 app.get("/about", (req, res) => {
   res.send("<h1>About Me</h1><p>My name is Angela</p>");
