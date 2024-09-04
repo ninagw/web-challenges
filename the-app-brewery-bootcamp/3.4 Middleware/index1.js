8c262f9300f83bce94969f767c624087c4e0ff39
Nina Weber <weber_nina@web.de>
2024-09-04 11:53:59 +0200

    updade index1.js / implement bodyParser

--- the-app-brewery-bootcamp/3.4 Middleware/index1.js
+++ the-app-brewery-bootcamp/3.4 Middleware/index1.js
@@ -1,3 +1,4 @@
+import bodyParser from "body-parser";
 import express from "express";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
@@ -6,9 +7,15 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/public/index.html");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post("/submit", (req, res) => {
+  console.log(req.body);
 });
+
+// app.get("/", (req, res) => {
+//   res.sendFile(__dirname + "/public/index.html");
+// });
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
