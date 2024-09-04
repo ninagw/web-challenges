a9be76cc1f0b2452f51721ffff33e4f7f30099c3
Nina Weber <weber_nina@web.de>
2024-09-04 12:05:34 +0200

    add new module of middleware

--- the-app-brewery-bootcamp/3.4 Middleware/index1.js
+++ the-app-brewery-bootcamp/3.4 Middleware/index1.js
@@ -9,13 +9,13 @@ const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
 app.post("/submit", (req, res) => {
   console.log(req.body);
 });
-
-// app.get("/", (req, res) => {
-//   res.sendFile(__dirname + "/public/index.html");
-// });
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
